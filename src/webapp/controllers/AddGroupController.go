e130717e691b8b0240ed9ee529acea4071c89222
杨活龙 <yanghuolong@zhisland.com>
2017-12-19 19:29:30 +0800

    wechat

--- src/webapp/controllers/AddGroupController.go
+++ src/webapp/controllers/AddGroupController.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	//	"time"
 	"yhl/help"
+	"yhl/wechat"
 )
 
 type AddGroupController struct {
@@ -9,7 +11,26 @@ type AddGroupController struct {
 }
 
 func (this *AddGroupController) Get() {
-	this.Data["welcome"] = "welcome to add group"
+	/*cache := help.Cache
+	token := cache.Get("access_token_")
+
+	if token == nil {
+		token = "ddsdsdsdssddsds"
+		cache.Put("access_token_", token, 100*time.Second)
+	} else {
+		token = string(token.([]uint8))
+	}
+
+	accessToken := token.(string) //wechat.GetAccessToken()
+	*/
+	accessToken := wechat.GetAccessToken()
+	m := map[string]interface{}{}
+	m["touser"] = "oTbmFxG5r1WRrHdb32O5y2aSAIkc"
+	m["msgtype"] = "text"
+	m["text"] = map[string]string{"content": "Hello World!"}
+	wechat.SendMsg(m)
+	wechat.SendTextMsg("oTbmFxG5r1WRrHdb32O5y2aSAIkc", "I love you")
+	this.Data["welcome"] = "welcome to add group, accessToken:" + accessToken
 	this.TplName = "addGroup/index.tpl"
 }
 
--- src/webapp/controllers/WechatController.go
+++ src/webapp/controllers/WechatController.go
@@ -1,28 +1,14 @@
 package controllers
 
 import (
-	"crypto/sha1"
 	"encoding/xml"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"sort"
 	"strings"
 	"time"
 	"yhl/help"
+	"yhl/wechat"
 )
-
-const Token = "feichangjuzu123456"
-
-type MsgBody struct {
-	XMLName      xml.Name `xml:"xml"`
-	ToUserName   string
-	FromUserName string
-	CreateTime   time.Duration
-	MsgType      string
-	Content      string
-	MsgId        int
-}
 
 type WechatController struct {
 	help.BaseController
@@ -32,7 +18,7 @@ func (this *WechatController) Index() {
 	timestamp, nonce, signatureIn := this.GetString("timestamp"), this.GetString("nonce"), this.GetString("signature")
 	echostr := strings.TrimSpace(this.GetString("echostr"))
 
-	if check(timestamp, nonce, signatureIn) {
+	if wechat.Check(timestamp, nonce, signatureIn) {
 		if len(echostr) > 0 {
 			this.Ctx.WriteString(echostr)
 			help.Log.Info("===== 接入成功 ======")
@@ -46,11 +32,11 @@ func (this *WechatController) Index() {
 
 		fmt.Println(string(body))
 
-		msgBody := &MsgBody{}
+		msgBody := &wechat.MsgBody{}
 		xml.Unmarshal(body, msgBody)
 		fmt.Println(msgBody)
 
-		replyBody := &MsgBody{}
+		replyBody := &wechat.MsgBody{}
 		replyBody.ToUserName = msgBody.FromUserName
 		replyBody.FromUserName = msgBody.ToUserName
 		replyBody.CreateTime = time.Since(time.Now())
@@ -62,15 +48,4 @@ func (this *WechatController) Index() {
 	}
 
 	this.StopRun()
-}
-
-func check(timestamp, nonce, signatureIn string) bool {
-	sl := []string{Token, timestamp, nonce}
-	sort.Strings(sl)
-	s := sha1.New()
-	io.WriteString(s, strings.Join(sl, ""))
-
-	signatureGen := fmt.Sprintf("%x", s.Sum(nil))
-
-	return signatureGen == signatureIn
 }
