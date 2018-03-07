3906ea4fd92fcec23c1e41e4b42d05a9d97f0bc5
杨活龙 <yanghuolong@zhisland.com>
2018-03-07 14:00:52 +0800

    mipo

---
+++ src/mipo/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ../../
+CURDIR=`pwd`
+OLDGOPATH="$GOPATH"
+echo "当前路径："$CURDIR
+echo "go原库路径："$OLDGOPATH
+
+export GOPATH="$CURDIR:$OLDGOPATH:/var/work/work_golang/mylib"
+echo "go新库路径:"$GOPATH
+
+cd ./src/mipo
+bee run
+#bee run  -gendoc=true -downdoc=true
+
+export GOPATH="$OLDGOPATH"
+
+echo "go恢复后库路径:"$GOPATH
+echo "finished"
