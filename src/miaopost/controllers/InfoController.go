91688fb8dd2dce9fa260588d7e0280540f2dcdfe
杨活龙 <yanghuolong@zhisland.com>
2018-03-08 18:02:40 +0800

    upload

--- src/miaopost/controllers/InfoController.go
+++ src/miaopost/controllers/InfoController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"miaopost/models"
 	"yhl/help"
 )
 
@@ -19,11 +20,48 @@ func (this *InfoController) List() {
 }
 
 // 创建页面
-func (this *InfoController) Create() {
+func (this *InfoController) CreateGet() {
+
 	this.TplName = "info/create.tpl"
+}
+
+func (this *InfoController) CreatePost() {
+	cid, _ := this.GetInt("cid")
+	content := this.GetString("content")
+	valid_day, _ := this.GetInt("valid_day")
+	email := this.GetString("email")
+	photo := this.GetString("photo")
+
+	info := new(models.Info)
+	info.Cid = int(cid)
+	info.Content = content
+	info.Valid_day = int(valid_day)
+	info.Email = email
+
+	is := models.CreateInfo(info)
+	if is {
+		_ = photo
+		this.SendRes(0, "success", nil)
+	}
+
+	this.SendRes(-1, "failed", nil)
 }
 
 // 展示页
 func (this *InfoController) View() {
 	this.TplName = "info/view.tpl"
+}
+
+// 编辑
+func (this *InfoController) EditGet() {
+	this.TplName = "info/edit.tpl"
+}
+
+func (this *InfoController) EditPost() {
+	this.SendRes(0, "", nil)
+}
+
+// 建议删除
+func (this *InfoController) SuggestDel() {
+	this.SendRes(0, "", nil)
 }
--- src/miaopost/routers/router.go
+++ src/miaopost/routers/router.go
@@ -3,10 +3,12 @@ package routers
 import (
 	"github.com/astaxie/beego"
 	"miaopost/controllers"
+	"yhl/api"
 )
 
 func init() {
 	beego.Router("/test", &controllers.TestController{})
 
 	beego.Router("/", &controllers.InfoController{})
+	beego.Router("/uploadfile", &api.UploadController{}, "post:Uploadfile")
 }
