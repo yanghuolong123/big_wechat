bfcee68d1d34dc12c3aae622af56323ad8c3edd1
杨活龙 <yanghuolong@zhisland.com>
2018-04-16 16:13:38 +0800

    js

--- src/miaopost/frontend/static/js/common.js
+++ src/miaopost/frontend/static/js/common.js
@@ -28,7 +28,7 @@ var prompt = function(option){
 		}
 	};
 
-function isWeiXin() {
+var isWeiXin = function() {
 	var ua = window.navigator.userAgent.toLowerCase();
 	//console.log(ua);//mozilla/5.0 (iphone; cpu iphone os 9_1 like mac os x) applewebkit/601.1.46 (khtml, like gecko)version/9.0 mobile/13b143 safari/601.1
 	if (ua.match(/MicroMessenger/i) == 'micromessenger') {
@@ -38,7 +38,7 @@ function isWeiXin() {
 	}
 }
 
-function sleep(milliSeconds){
+var sleep = function(milliSeconds){
     var startTime = new Date().getTime(); // get the current time
     while (new Date().getTime() < startTime + milliSeconds); // hog cpu
 }
@@ -125,4 +125,80 @@ var alerting = function(settings) {
         });
     }
     modalEle.modal('show');
+}
+
+var isNum = function(c) {
+    if(!/^[0-9]+$/.test(c)){
+        return false;
+    }
+
+    return true;
+}
+
+var isUrl = function CheckUrl(str) { 
+    var RegUrl = new RegExp(); 
+    RegUrl.compile("^[A-Za-z]+://[A-Za-z0-9-_]+\\.[A-Za-z0-9-_%&\?\/.=]+$");
+    if (!RegUrl.test(str)) { 
+    return false; 
+    } 
+    return true; 
+} 
+
+/*
+ * 图片 upload 
+ */
+/*
+ * settting字段
+ * before: 上传前调用的函数
+ * sucess: 上传成功后调用的函数
+ */
+function PicUploader(settings) {
+    var inputId = 'picupload-' + Math.random().toString().slice(2);
+    var self = this;
+    this.settings = settings;
+    //this.$fileInput = $('<input type="file" accept=".jpg,.png,.jpeg,.gif" id="' + inputId + '" style="display:none;">');
+    this.$fileInput = $('<input type="file" accept="image/*" id="' + inputId + '" style="display:none;">');
+    this.$fileInput.on('change', function() {
+        self._onChange(self);
+    });
+    $('body').append(this.$fileInput);
+}
+
+PicUploader.prototype = {
+    consotructor: PicUploader,
+    $fileInput: null,
+    settings: {},
+    /*
+     * 开始上传操作
+     */
+    start: function() {
+        this.$fileInput.click();
+    },
+    _onChange: function(self) {
+        var picFormData = new FormData();
+        picFormData.append('file', this.$fileInput.get(0).files[0]);
+        /*
+         * 上传前
+         */
+        self.settings['before'] && self.settings['before']();
+
+        $.ajax({
+            url: '/uploadfile',
+            type: 'post',
+            data: picFormData,
+            dataType: 'json',
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(obj) {
+                if (obj.code == 0) {
+                    /*
+                     * 上传成功
+                     */
+                    self.settings['success'] && self.settings['success'](obj);
+                } else {
+                }
+            }
+        });
+    }
 }
