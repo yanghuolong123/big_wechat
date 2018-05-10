c1fec2928b69e04a821268705bf7133cbedb1c37
杨活龙 <yanghuolong@zhisland.com>
2018-05-10 10:04:17 +0800

    session

--- src/miaopost/frontend/controllers/BaseController.go
+++ src/miaopost/frontend/controllers/BaseController.go
@@ -21,9 +21,6 @@ func (this *BaseController) Prepare() {
 	cats := models.GetAllCategory()
 	this.Data["cats"] = cats
 
-	user := this.GetSession("user")
-	this.Data["user"] = user
-
 	footer_nav := models.GetArticleByType(models.Type_Nav)
 	this.Data["footer_nav"] = footer_nav
 	this.Data["last_footer_nav_index"] = len(footer_nav) - 1
@@ -73,6 +70,9 @@ func (this *BaseController) Prepare() {
 		this.Data["signPackage"] = signPackage
 		this.Data["wxshare"] = WxShare
 	}
+
+	user := this.GetSession("user")
+	this.Data["user"] = user
 
 	this.BaseController.Prepare()
 }
