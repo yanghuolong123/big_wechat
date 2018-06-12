267be9d1783b60bc698710f5c60b2bff8f7d570b
杨活龙 <yanghuolong@zhisland.com>
2018-06-12 13:39:00 +0800

    login

--- src/miaopost/frontend/static/js/main.js
+++ src/miaopost/frontend/static/js/main.js
@@ -249,11 +249,11 @@ $(function(){
 			            clearInterval(timer);
 			            if(e.code==0) {
 			            	//window.location = "/user";
-			            	prompt({msg:"已登录！此时通过电脑或公众号发布后都可以进行编辑、删除和免费置顶操作",displayTime:3000});
-		                        	setTimeout(function(){
-		                        		window.location = "/";
-		                        	}, 2500);
-			            	return;
+			            	// prompt({msg:"已登录！此时通过电脑或公众号发布后都可以进行编辑、删除和免费置顶操作",displayTime:3000});
+		              //           	setTimeout(function(){
+		              //           		window.location = "/";
+		              //           	}, 2500);
+			            	// return;
 			            }
 
 			            window.location = "/";
